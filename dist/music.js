6570001adfc1c1476badcc038358168d4bfda0a4
fengliqiu <47531370+fengliqiu@users.noreply.github.com>
2019-05-21 19:36:04 +0800

    Site updated: 2019-05-21 19:35:59

--- dist/music.js
+++ dist/music.js
@@ -1,13 +1,3 @@
-const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    fixed: true,
-    autoplay: false,
-    audio: [
-      {
-        name: "Last Christmas",
-        artist: 'Taylor Swift',
-        url: 'https://music.163.com/song?id=2002351&userid=1592076880',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
-      }
-    ]
-});
+// build time:Tue May 21 2019 19:35:52 GMT+0800 (GMT+08:00)
+const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,audio:[{name:"Last Christmas",artist:"Taylor Swift",url:"https://music.163.com/song?id=2002351&userid=1592076880",cover:"http://oeff2vktt.bkt.clouddn.com/image/84.jpg"}]});
+//rebuild by neat 
