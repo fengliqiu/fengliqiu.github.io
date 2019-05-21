952f3d61fa475cb5b086ff55211493d81080cc50
fengliqiu <47531370+fengliqiu@users.noreply.github.com>
2019-05-21 19:48:11 +0800

    Site updated: 2019-05-21 19:48:08

--- dist/music.js
+++
@@ -1,3 +0,0 @@
-// build time:Tue May 21 2019 19:41:38 GMT+0800 (GMT+08:00)
-const ap=new APlayer({container:document.getElementById("aplayer"),fixed:true,autoplay:false,audio:[{name:"Last Christmas",artist:"Taylor Swift",url:"https://music.163.com/song?id=2002351&userid=1592076880",cover:"http://oeff2vktt.bkt.clouddn.com/image/84.jpg"}]});
-//rebuild by neat 
