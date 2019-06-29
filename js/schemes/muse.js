ccf766efa4da20b32fa4092d09ec8cab879fdd5c
fengliqiu <47531370+fengliqiu@users.noreply.github.com>
2019-06-29 17:48:50 +0800

    Site updated: 2019-06-29 17:48:45

--- js/schemes/muse.js
+++ js/schemes/muse.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 29 2019 17:44:21 GMT+0800 (GMT+08:00)
+// build time:Sat Jun 29 2019 17:48:39 GMT+0800 (GMT+08:00)
 $(document).ready(function(){function t(){var t=$("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(true):$(".container").height();if(t<window.innerHeight)$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed");else $("#footer").removeAttr("style position")}t();$(window).on("resize scroll",t)});
 //rebuild by neat 
